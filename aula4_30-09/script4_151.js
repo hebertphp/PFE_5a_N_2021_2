e154a089bdb90d6a18350439486855280782fed5
Hebert <hebertphp@gmail.com>
2021-09-30 21:12:44 -0300

    PFE_5a 30/09

--- aula4_30-09/script4_151.js
+++ aula4_30-09/script4_151.js
@@ -1,8 +1,10 @@
 var senha, passw, res;
+senha=prompt("digite sua senha");
 passw="1234";
-res=(passw==1234);//true (verdadeiro) false(falso)
+res=!(senha==passw);//true (verdadeiro) false(falso)
 document.write(res);
 /*
 ==  iguais
 === identicos
+! not
 */
