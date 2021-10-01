d6d25a23c73119dcf8405b65f741cbc9fcd997d8
Hebert <hebertphp@gmail.com>
2021-09-30 21:43:10 -0300

    PFE_5a 30/09

--- aula4_30-09/script4_161.js
+++ aula4_30-09/script4_161.js
@@ -2,8 +2,8 @@ var a,b,c,d;
 a=50;
 b=120;
 c=200;
-d=(a<=b) && (a>=c);//
-document.write("d = "+d);
+d=(a<=b) && (a>=c);//d=(true) && (false)  //d=false
+document.write("d = "+ d);//d = false
 /*
 
 
---
+++ aula4_30-09/script4_162.js
@@ -0,0 +1,7 @@
+var usuario, usu, senha, senh, res;
+var usu=prompt("Digite o usuario");
+var senh=prompt("Digite a senha");
+usuario = "pera";
+senha = "qwert";
+
+document.write(res);
