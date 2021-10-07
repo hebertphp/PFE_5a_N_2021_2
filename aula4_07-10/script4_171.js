0888975f02ef4eccc4ee3d7bd56cdd1ab46b3e3b
Hebert <hebertphp@gmail.com>
2021-10-07 20:48:23 -0300

    PFE_5a 07/10

--- aula4_07-10/script4_171.js
+++ aula4_07-10/script4_171.js
@@ -1,9 +1,10 @@
-var a,b,c,d;
+var a,b,c,d,e;
 a=50;
 b=120;
 c=200;
-d=!((a<=b) || (a>=c));//d=(T)||(F) //d=()
-document.write("d = "+d);//d = 
+d=!((a<=b) || (a>=c));//d=!((T)||(F)) //d=!(T)//d=F
+e=!((5>=8) || (7>=9));//e=
+document.write("e = "+e);
 /*
 
 */
