86bafcbd457d6356c21d698b914b316652b2663b
Hebert <hebertphp@gmail.com>
2021-10-14 20:27:24 -0300

    PFE_5a 14/10

--- aula5_14-10/script5_01.js
+++ aula5_14-10/script5_01.js
@@ -1,7 +1,14 @@
 var a,b;
-a = 5;
+a =  5;
 b = "5";
-if (a == b) {
+if (a === b) {
   document.write("a = "+a+", b = "+b+"<br>");
   document.write("A variável a é igual a variável b");
 }
+
+document.write("<br> E agora?");
+/*
+==  igualdade
+=== idênticos
+
+*/
