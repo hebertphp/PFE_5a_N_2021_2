8f27bf4c4a8f9da503eba7fd09012c442bf042f6
Hebert <hebertphp@gmail.com>
2021-10-21 20:11:22 -0300

    PFE_5a 21/10/21

---
+++ aula5_14-10/script5_061.js
@@ -0,0 +1,15 @@
+var a = parseInt(prompt("Digite o Valor de A"));
+var b = parseInt(prompt("Digite o Valor de B"));
+
+if (a == b){
+
+    document.write("A Variável A (" + a + ") é igual a variável B (" + b +").");
+} else if (a < b){
+
+    document.write("A Variável A (" + a + ") é menor que a variável B (" + b +").");
+
+} else {
+
+    document.write("A Variável A (" + a + ") é maior que a variável B (" + b +").");
+
+}
