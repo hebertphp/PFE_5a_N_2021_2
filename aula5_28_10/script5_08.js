214f322232b640d9365b3db4737333bb27fa8a7d
Hebert <hebertphp@gmail.com>
2021-10-28 21:04:21 -0300

    PFE_5a 28/10/21

--- aula5_28_10/script5_08.js
+++ aula5_28_10/script5_08.js
@@ -1,14 +1,30 @@
 var i;
-i=3; // inicialização do contador;
+i=0; 
 while (i <= 5) {
  document.write(i + "<br>");
  i++;
 }
 /*
-1) Qual o valor inicial? 3
+(i++) 
+        i       i<=5     saída
+        0       V(T)    0
+        1       V(T)    1
+        2       V(T)    2
+        3       V(T)    3
+        4       V(T)    4
+        5       V(T)    5
+        6       Falso
+
+1) Qual o valor inicial? 0 
 2) Qual a condição? i <= 5
-3) Qual o contador? i++ (somar 1)
-4) Qual o valor que torna a condição falsa?6
-5) Quantas vezes foi executado o looping?3
+3) Qual o contador? i++
+4) Qual o valor que torna a condição falsa? 6
+5) Quantas vezes foi executado o looping? 6
 6) Qual o último valor impresso? 5
+
+Desafio 1:
+
+Desafio 2:
+
+Desafio 3:
 */
