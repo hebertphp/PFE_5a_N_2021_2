00c318c0820b01bc336bb4501115e3d33474a16c
Hebert <hebertphp@gmail.com>
2021-11-04 20:43:26 -0300

    PFE_5a 04/11/21

--- aula5_04_11/script5_09.js
+++ aula5_04_11/script5_09.js
@@ -1,6 +1,17 @@
 var i;
-i=0; // inicialização do contador;
+i=0; //VI
 do{
-  document.write(i + "<br>");
-  i++;
-}while (i < 3);
+  document.write(i + " ");
+  i++;//Cont.
+}while (i < 3);//Cond. está no final
+document.write("<hr>O valor que tornou a condição como falsa foi: "+i)
+/*
+Saída: 
+Quais os valores impressos? 
+Quantas vezes foi executado o looping? 
+Qual o valor que tornou a condição como falsa? 
+Como fazer para mostrar a mensagem:
+
+O valor que tornou a condição como falsa foi: __
+
+*/
