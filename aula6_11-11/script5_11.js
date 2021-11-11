7e49fcd793228fe7a13de5600ba5dac678dd4752
Hebert <hebertphp@gmail.com>
2021-11-11 20:14:27 -0300

    PFE_5a 11/11/21

--- aula6_11-11/script5_11.js
+++ aula6_11-11/script5_11.js
@@ -1,6 +1,13 @@
-var i,j;
-for (i=0; i<3; i++) {
- for (j=0; j<4; j++) {
-   document.write(i+" "+j+"<br>");
- }
+var i,j,k;
+for (i=0; i<2; i++) { 
+    for (j=0; j<3; j++) {
+        for(k=0; k<4; k++){
+            document.write(i+" "+j+" "+k+"<br>");
+        } 
+    }
 }
+/*
+    i   j   k
+   
+
+*/
