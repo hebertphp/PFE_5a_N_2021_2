6ce37f7550d8a6c1f22335c922b0277b6f7dbc15
Hebert <hebertphp@gmail.com>
2021-11-18 20:54:57 -0300

    PFE_5a 18/11/21

--- aula6_18-11/script6_04.js
+++ aula6_18-11/script6_04.js
@@ -1,11 +1,21 @@
 var x=[2020,2,"Hoje"];
+var msg="";
 x[0]++;/* soma 1 */
 x[1]=x[0]%2;
-x[0]+=" em dia";/* operador de atribuição Juntar o valor que a posição já tem concatenando(juntando) com em dia */;
+x[2]+=" em dia";/* operador de atribuição Juntar o valor que a posição já tem concatenando(juntando) com em dia */;
 
-document.getElementById("saida").innerHTML=x[0]+" <br>"+ x[1]+ " <br>"+ x[2]+ " <br>";
+msg+=x[0]+"<hr>";
+msg+=x[1]+"<hr>";/*   */
+msg+=x[2]+"<br>";/* para comentar uma linha [Ctrl][;] */
+msg+=x;
+
+document.getElementById("saida").innerHTML=msg;
+
+// document.getElementById("saida").innerHTML=x[0]+" <br>"+ x[1]+ " <br>"+ x[2]+ " <br>";
 /*
 x[0]=2021
 x[1]=1
 x[2]=Hoje em dia
+
+Um minuto...
 */
---
+++ aula6_18-11/script6_05.js
@@ -0,0 +1,14 @@
+var msg="";
+var nota=[10,8.5,7.75,9.5];
+var num=[];
+num[0]=120;
+num[1]=200;
+num[2]=2020;
+
+var nome=new Array("Jose","Paula","Carla");
+
+msg+=nota+"<hr>";
+msg+=num+"<hr>";
+msg+=nome+"<hr>";
+
+document.querySelector("#saida").innerHTML=msg;
