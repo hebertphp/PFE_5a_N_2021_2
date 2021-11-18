4d1eff0d2124a43c5842b6485e3ccf44b79a1725
Hebert <hebertphp@gmail.com>
2021-11-18 19:25:41 -0300

    PFE_5a 18/11/21

---
+++ aula6_18-11/script5_11.js
@@ -0,0 +1,13 @@
+var i,j,k;
+for (i=0; i<2; i++) { //ponteiro hora
+    for (j=0; j<3; j++) {//ponteiro minuto
+        for(k=0; k<4; k++){//ponteiro segundo
+            document.write(i+" "+j+" "+k+"<br>");
+        } 
+    }
+}
+/*
+    i   j   k
+    0   0   0
+
+*/
---
+++ aula6_18-11/script6_01.js
@@ -0,0 +1,13 @@
+var uf = ["SP","RJ","MG","ES"];
+var notas =[10, 9.5, 8, 7.5];
+ document.getElementById("saida").innerHTML=notas;
+//document.write(notas);
+/*
+Criar uma variável do tipo array nomeada notas, 
+contendo 4 notas, valores numericos, nessa variável
+Números: 
+    Inteiros:               10    2021
+    Pto Flutuante, real     9.5     7.5
+    "123" + 5 = "1235"
+     123  + 5 = 128
+*/
