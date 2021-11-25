bd2a0788cd7f179ffddf205f92783cc1e14224c2
Hebert <hebertphp@gmail.com>
2021-11-25 20:12:42 -0300

    PFE_5a 25/11/21

---
+++ aula6_25-11/script6_061.js
@@ -0,0 +1,7 @@
+var msg="";
+var animal=["gato","lobo","cavalo","lagarto","ornitorrinco"];
+
+msg=animal[3];
+document.querySelector(".d1").innerHTML=msg;
+
+document.getElementById("saida").innerHTML=animal[2];
