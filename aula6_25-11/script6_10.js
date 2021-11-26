7c766c2cd0b24a3dbdcf80534092d50ac4a5343c
Hebert <hebertphp@gmail.com>
2021-11-25 21:57:03 -0300

    PFE_5a 25/11/21

---
+++ aula6_25-11/script6_10.js
@@ -0,0 +1,3 @@
+var x = [13,05,2021];
+var novaLista = x.join("/");
+document.querySelector(".d1").innerHTML=novaLista;
---
+++ aula6_25-11/script6_11.js
@@ -0,0 +1,7 @@
+var notas=[10,9,8];
+var outras_notas=[4,3,2,1];
+document.write(notas+"<hr>");
+var novas_notas=notas.concat(7,6,5);
+document.write(novas_notas+"<hr>");
+var junta_tudo=novas_notas.concat(outras_notas);
+document.write(junta_tudo+"<hr>");
