cdbbf7b4a584bcb788a8fb5922bfec8581e0f5a4
Hebert <hebertphp@gmail.com>
2021-12-16 19:59:06 -0300

    PFE_5a 16/12/21

---
+++ aula7/aula7_01.js
@@ -0,0 +1,10 @@
+/* -----------------------------------------
+function boas_vindas()
+Objetivo: Enviar mensagem de boas vindas
+Data de criação: 16/05/2021
+Autor: Nome do autor
+-------------------------------------------- */
+function boas_vindas() {
+    document.write("Seja bem vindo!");
+}
+boas_vindas();
