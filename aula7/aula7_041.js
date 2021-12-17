5deedeae980be89e1ce34e8acd1b1fc2740cfb3d
Hebert <hebertphp@gmail.com>
2021-12-16 21:05:47 -0300

    PFE_5a 16/12/21

--- aula7/aula7_041.js
+++ aula7/aula7_041.js
@@ -1,9 +1,18 @@
-function calc(n1,n2){
-    return n1+n2;
+function calc(n1,n2,oper){
+    if(oper=="+")
+        return n1+n2;
+    else if(oper=="-")
+        return n1-n2;
+    else if(oper=="*")
+        return n1*n2;    
+    else    
+        return "Operador não encontrado";     
 }
     var num1=parseFloat(prompt("Digite um número"));
     var num2=parseFloat(prompt("Digite outro número"));
-    document.write("num1: "+num1+", num2: "+num2+" calc: "+calc(num1,num2));
+    var operador=prompt("Digite o operador");
+
+    document.write("num1: "+num1+", num2: "+num2+" calc: "+calc(num1,num2,operador));
 
     /*
 Desafio: Alterar a função calc() implementando um terceiro parâmetro como operador
