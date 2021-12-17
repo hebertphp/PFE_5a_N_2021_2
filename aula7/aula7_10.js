77c751d51e998bcd119b4297b7061412637f9e38
Hebert <hebertphp@gmail.com>
2021-12-16 21:44:29 -0300

    PFE_5a 16/12/21

---
+++ aula7/aula7_10.js
@@ -0,0 +1,14 @@
+function par_impar(){
+    document.write(x);
+    if(x%2==0){
+        let msg=" PAR<br>";//escopo local/bloco
+    }
+    else{
+        var msg=" ÍMPAR<br>";//escopo local/bloco
+    }
+    document.write(msg);
+}
+    var x=28;//escopo global
+    par_impar();document.write("<hr>");
+    var x=29;//escopo global
+    par_impar();
